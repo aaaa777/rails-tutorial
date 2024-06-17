9e8822c2b815c166fc022019e5f927b5206ea1fa
aaaa777 <aaaa777@users.noreply.github.com>
2024-06-17 16:19:02 +0900

    add: named routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'foo/bar'
   get 'foo/baz'
-  get 'static_pages/home'
-  get 'static_pages/help'
   resources :microposts
   resources :users
   root "static_pages#home"
-  get 'static_pages/about'
+  get  "/help",    to: "static_pages#help"
+  get  "/about",   to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact"
 end
