c0261be9ba4b6113feba0db2e75f16235ad29aae
aaaa777 <aaaa777@users.noreply.github.com>
2024-07-13 23:35:41 +0900

    add feature: user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
-  resources :microposts
-  # resources :users
+  # get 'foo/bar'
+  # get 'foo/baz'
+  # resources :microposts
+  resources :users
   root "static_pages#home"
   get  "/help",    to: "static_pages#help"
   get  "/about",   to: "static_pages#about"
