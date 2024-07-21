8c3823da54e0c38b0abe27a96057f2c591685b14
aaaa777 <aaaa777@users.noreply.github.com>
2024-07-22 03:45:03 +0900

    add: admin operation and pagenation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -30,6 +30,38 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              "password",
+                                            password_confirmation: "password",
+                                            admin: 1 } }
+    assert_not @other_user.reload.admin?
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to root_url
+  end
+  
   # test "should create user" do
   #   assert_difference("User.count") do
   #     post users_url, params: { user: { email: @user.email, name: @user.name } }
